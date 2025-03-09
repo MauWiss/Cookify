d7531aa73f33b0c0429135145c59130f07949426
Ran Almagor <ranalmagor100@gmail.com>
2025-03-09 16:12:53 +0200

    Ran

--- HomeChef/homechef_finalproject/src/App.jsx
+++ HomeChef/homechef_finalproject/src/App.jsx
@@ -1,5 +1,5 @@
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
-import Home from "./pages/home";
+import Home from "./pages/Homepage";
 import Favorites from "./pages/Favorites";
 import MyRecipes from "./pages/MyRecipes";
 import AddRecipe from "./pages/AddRecipe";
--- HomeChef/homechef_finalproject/src/Welcome.jsx
+++
@@ -1,5 +0,0 @@
-function Welcome() {
-  return <h1>ברוך הבא לאפליקציה שלי!</h1>;
-}
-
-export default Welcome;
