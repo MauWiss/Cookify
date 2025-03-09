5ef007df04efbdae16948e833b39432802370bb0
Ran Almagor <ranalmagor100@gmail.com>
2025-03-09 16:09:56 +0200

    Ran
    09.03.25 - 16:09

--- HomeChef/homechef_finalproject/src/App.jsx
+++ HomeChef/homechef_finalproject/src/App.jsx
@@ -1,36 +1,37 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
-import Welcome from "./Welcome"; // ייבוא הקומפוננט החדש
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import Home from "./pages/home";
+import Favorites from "./pages/Favorites";
+import MyRecipes from "./pages/MyRecipes";
+import AddRecipe from "./pages/AddRecipe";
+import Meal from "./pages/Meal";
+import EditRecipe from "./pages/EditRecipe";
+import Cooks from "./pages/Cooks";
+import Login from "./pages/Login";
+import NotFound from "./pages/NotFound";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
-    <>
-      <div>
-        <Welcome />
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <Router>
+      <nav>
+        <Link to="/">Home</Link> | <Link to="/favorites">Favorites</Link> |
+        <Link to="/my-recipes">My Recipes</Link> |{" "}
+        <Link to="/add-recipe">Add Recipe</Link> |<Link to="/cooks">Cooks</Link>{" "}
+        | <Link to="/login">Login</Link>
+      </nav>
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/favorites" element={<Favorites />} />
+        <Route path="/my-recipes" element={<MyRecipes />} />
+        <Route path="/add-recipe" element={<AddRecipe />} />
+        <Route path="/meal/:id" element={<Meal />} /> {/* יטען מתכון לפי ID */}
+        <Route path="/edit-recipe/:id" element={<EditRecipe />} />
+        <Route path="/cooks" element={<Cooks />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="*" element={<NotFound />} />{" "}
+        {/* דף ברירת מחדל לכתובות לא קיימות */}
+      </Routes>
+    </Router>
   );
 }
 
