e3dbcd2cbcdc02bb889253a8bd439d1cb6ffa977
Ran Almagor <ranalmagor100@gmail.com>
2025-04-04 15:40:49 +0300

    Ran

--- HomeChef/HomeChefServer/Program.cs
+++ HomeChef/HomeChefServer/Program.cs
@@ -1,37 +1,25 @@
-﻿using Microsoft.EntityFrameworkCore;
-using HomeChefServer.Data;
-using Microsoft.AspNetCore.Diagnostics.HealthChecks;
+﻿using HomeChefServer.Data;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Connection  
+// Add services
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
-// Controllers
 builder.Services.AddControllers();
-
-// Swagger
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-// Health Checks
-builder.Services.AddHealthChecks()
-    .AddSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
-
 var app = builder.Build();
 
-// Swagger (Development only)
+// Middleware
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
 }
 
-// Controllers Mapping
+app.UseAuthorization();
 app.MapControllers();
-
-// Health check endpoint
-app.MapHealthChecks("/health");
-
 app.Run();
