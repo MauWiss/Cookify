33ebbeaeecfb3b4b80fd3382263677fc90629a34
Ran Almagor <ranalmagor100@gmail.com>
2025-04-04 15:15:50 +0300

    Ran

--- HomeChef/HomeChefServer/Program.cs
+++ HomeChef/HomeChefServer/Program.cs
@@ -4,20 +4,25 @@ using Microsoft.AspNetCore.Diagnostics.HealthChecks;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// חיבור למסד נתונים
+//COnnection  
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
-// הוספת Controllers
+//  Controllers
 builder.Services.AddControllers();
 
-// הוספת Health Checks
+// Health Checks
 builder.Services.AddHealthChecks()
     .AddSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+/*
+ //https://localhost:5007/health
+ //https://localhost:7019/health
+ */
+
 
 var app = builder.Build();
 
-// מיפוי Controllers
+//  Controllers Mapping
 app.MapControllers();
 
 // מיפוי בדיקת בריאות למסד הנתונים
