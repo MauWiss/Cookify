47a23cc884f0627b86323ac0151df083ac97e5cf
Ran Almagor <ranalmagor100@gmail.com>
2025-04-10 20:09:06 +0300

    Ran -Review

---
+++ HomeChef/HomeChefServer/Models/DTOs/ReviewDTO.cs
@@ -0,0 +1,11 @@
+﻿namespace HomeChefServer.DTOs
+{
+    public class ReviewDTO
+    {
+        public int ReviewId { get; set; }
+        public int UserId { get; set; }
+        public string Username { get; set; } // שם המשתמש להצגה בביקורות
+        public string ReviewText { get; set; }
+        public DateTime CreatedAt { get; set; }
+    }
+}
