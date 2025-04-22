8dc2556b4181b861bf466435de3a4b1ea4253c3b
Ran Almagor <ranalmagor100@gmail.com>
2025-04-22 23:03:01 +0300

    Update main.jsx

--- HomeChef/Homechef_Customer/src/main.jsx
+++ HomeChef/Homechef_Customer/src/main.jsx
@@ -2,13 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom"; // שים לב שהחלפנו ל-HashRouter
 import "@fortawesome/fontawesome-free/css/all.min.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter basename="/cgroup82/tar1">
+    <HashRouter basename="/cgroup82/tar1">
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>,
 );
